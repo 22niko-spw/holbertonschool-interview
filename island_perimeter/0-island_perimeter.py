fab5a3e0144de67dd7a47cd4c6daae9a1b0c2325
22niko-spw <nicosojd@gmail.com>
2026-07-08 09:46:31 +0200

    fix: wrong fichier

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 """
-0-main
+Module 0-island_perimeter
+Determines the perimeter of the island described in grid.
 """
-island_perimeter = __import__('0-island_perimeter').island_perimeter
 
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
+
+def island_perimeter(grid):
+    """
+    Return the perimeter of the island described in grid.
+
+    Args:
+        grid (list): list of list of integers where
+                     0 represents water and 1 represents land
+
+    Returns:
+        int: the perimeter of the island
+    """
+    perimeter = 0
+    height = len(grid)
+
+    for row in range(height):
+        width = len(grid[row])
+        for col in range(width):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
